a1f9eec4a3595bb81432f38cd9c119380fc19331
kanabot78 <166187885+kanabot78@users.noreply.github.com>
2024-04-06 10:04:12 +0200

    Update اعفاء.js

--- plugins/اعفاء.js
+++ plugins/اعفاء.js
@@ -20,7 +20,7 @@ var user = number + '@s.whatsapp.net'
 } catch (e) {
 } finally {
 conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-m.reply(`*تـــم الــأعــفـاء !*`)
+m.reply(`*تـــم الــأعــفـاء ! قلولي شطوره انا قطوتكم مياو 😺*`)
 }
 
 }
