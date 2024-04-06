7e8f4de481b1978f82f206c3728c4d02b9becfff
kanabot78 <166187885+kanabot78@users.noreply.github.com>
2024-04-06 09:50:32 +0200

    Update الجروبات.js

--- plugins/الجروبات.js
+++ plugins/الجروبات.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let txt = ''
 for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `\n—◉ ${await conn.getName(jid)}\n➤ ${jid} [${chat?.metadata?.read_only ? 'لا يوجد مشاركون' : 'مشارك'}]\n\n`
-m.reply(`*قائمة القروبات التي ميجو بداخلها:*
+m.reply(`*قائمة الجروبات الي كانا القيرل الدلوعه🥺 فيها:*
 ${txt}
 `.trim())
 }
