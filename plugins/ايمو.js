52f5e395017dd3b06123e670022d36bc18683592
numberrina <167228677+numberrina@users.noreply.github.com>
2024-04-29 02:53:37 +0300

    Update ايمو.js

--- plugins/ايمو.js
+++ plugins/ايمو.js
@@ -3,7 +3,7 @@ conn.reply(m.chat,`**\n\n*“${pickRandom(global.verdaad)}”*\n\n**`, m)
 }
 handler.help = ['ايمو']
 handler.tags = ['fun']
-handler.command = /^ايمو/i
+handler.command = /^ايموجي/i
 export default handler
 function pickRandom(list) {
 return list[Math.floor(list.length * Math.random())]
