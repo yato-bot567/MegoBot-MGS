ee84c6c9a5aef29832d794dda6eaf823c359364a
numberrina <167228677+numberrina@users.noreply.github.com>
2024-04-29 02:36:58 +0300

    Create علم.js

---
+++ plugins/علم.js
@@ -0,0 +1,36 @@
+//BY KUROSAKI 
+//كود من طرف كوروساكي 
+//لينك القنات : https://whatsapp.com/channel/0029VaSJqny2kNFyDSt8dM2r
+//+212669512443
+//🧧🗿🗿🗿🗿🗿
+let timeout = 60000
+let poin = 500
+let handler = async (m, { conn, command, usedPrefix }) => {
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    let id = m.chat
+    if (id in conn.tebakbendera) {
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
+        throw false
+    }
+    let src = await (await fetch('https://raw.githubusercontent.com/Hjfjckb/Kurosaki/main/Kurosaki.json')).json()
+  let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `*${command.toUpperCase()}*
+  ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
+  *استخدم .انسحب للأنسحاب*
+  ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
+*❰BY : KUROSAKI🧧❱*
+     `.trim()
+    conn.tebakbendera[id] = [
+        await conn.sendFile(m.chat, json.question, '', caption, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.response}*┇`, conn.tebakbendera[id][0])
+            delete conn.tebakbendera[id]
+        }, timeout)
+    ]
+}
+handler.help = ['guessflag']
+handler.tags = ['game']
+handler.command = /^علم/i
+
+export default handler
