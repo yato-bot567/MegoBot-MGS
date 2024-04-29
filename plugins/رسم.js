e9a1c8d3e99ff906bd98ee08e559eeb2b25ef535
numberrina <167228677+numberrina@users.noreply.github.com>
2024-04-29 05:56:02 +0300

    Update رسم.js

--- plugins/رسم.js
+++ plugins/رسم.js
@@ -22,5 +22,5 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 handler.help = ['dalle'];
 handler.tags = ['drawing'];
-handler.command = ['ارسم'];
+handler.command = ['رسم'];
 export default handler;
