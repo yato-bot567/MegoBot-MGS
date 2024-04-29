8dd6b2d5cc8c29b8eae73b782b663f6e3d79fd2e
numberrina <167228677+numberrina@users.noreply.github.com>
2024-04-29 03:08:57 +0300

    Update منشني.js

--- plugins/منشني.js
+++ plugins/منشني.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text }) => {
-  let tagme = `https://wa.me/+${m.sender.replace(`+`)}/?text=BY+Aboud+Coding`
+  let tagme = `https://wa.me/+${m.sender.replace(`+`)}/?text=بواسطة+ابو+عبدالله`
   let mylink = [m.sender]
   conn.reply(m.chat, tagme, m, { contextInfo: { mylink }})
 }
