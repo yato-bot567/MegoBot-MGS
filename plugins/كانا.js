db0297d61cdc5b876d4b5d50cdd5b14181b7d2eb
numberrina <167228677+numberrina@users.noreply.github.com>
2024-05-02 23:25:57 +0300

    Update كانا.js

--- plugins/كانا.js
+++ plugins/كانا.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/toma.mp3'
 conn.sendFile(m.chat, vn, 'toma.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
-handler.command = /^(:c|c)$/i
+handler.command = /^(:كانا)$/i
 handler.fail = null
 handler.exp = 100
 export default handler*/
