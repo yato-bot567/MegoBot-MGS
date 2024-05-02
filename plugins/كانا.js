79d9b25e92db83be3fb068476261024e808ad211
numberrina <167228677+numberrina@users.noreply.github.com>
2024-05-02 23:33:17 +0300

    Update كانا.js

--- plugins/كانا.js
+++ plugins/كانا.js
@@ -7,7 +7,7 @@ const handler = async (m, {conn, usedPrefix}) => {
   conn.sendPresenceUpdate('recording', m.chat);
   conn.sendMessage(m.chat, {audio: {url: vn}, ptt: true, mimetype: 'audio/mpeg', fileName: `deja de llorar.mp3`}, {quoted: m});
 };
-handler.command = /^(كانا)$/i;
+handler.command = /^كانا$/i;
 handler.fail = null;
 handler.exp = 100;
 export default handler; 
@@ -19,7 +19,7 @@ let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/toma.mp3'
 conn.sendFile(m.chat, vn, 'toma.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
-handler.command = /^(:كانا)$/i
+handler.command = /^(:c|c)$/i
 handler.fail = null
 handler.exp = 100
 export default handler*/
