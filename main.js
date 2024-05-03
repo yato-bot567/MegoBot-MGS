5911f6551119c9df8f1c85206db000fa4a73e6ec
numberrina <167228677+numberrina@users.noreply.github.com>
2024-05-03 09:18:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -545,7 +545,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `البوت شغال بواسطه‍ه ميجو :\n\n ${uptime}`;
+  const bio = `البوت شغال بواسطه‍ه ابو عبدالله :\n\n ${uptime}`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
