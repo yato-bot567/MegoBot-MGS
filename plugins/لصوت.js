58ed72b26e186cf7654eca71cbf500179bb3ad9b
numberrina <167228677+numberrina@users.noreply.github.com>
2024-05-03 09:04:13 +0300

    Update لصوت.js

--- plugins/لصوت.js
+++ plugins/لصوت.js
@@ -1,21 +1,14 @@
-import {toAudio} from '../lib/converter.js';
-
-const handler = async (m, {conn, usedPrefix, command}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.convertidor_tomp3
-
-
-  const q = m.quoted ? m.quoted : m;
-  const mime = (q || q.msg).mimetype || q.mediaType || '';
-  if (!/video|audio/.test(mime)) throw `*${tradutor.texto1}*`;
-  const media = await q.download();
-  if (!media) throw `*${tradutor.texto2}*`;
-  const audio = await toAudio(media, 'mp4');
-  if (!audio.data) throw `*${tradutor.texto3}*`;
-  conn.sendMessage(m.chat, {audio: audio.data, mimetype: 'audio/mpeg'}, {quoted: m});
-};
-handler.alias = ['tomp3', 'لصوت'];
-handler.command = /^to(mp3|لصوت)$/i;
-export default handler;
+import { toAudio } from '../lib/converter.js'
+let handler = async (m, { conn, usedPrefix, command }) => {
+let q = m.quoted ? m.quoted : m
+let mime = (q || q.msg).mimetype || q.mediaType || ''
+if (!/video|audio/.test(mime)) throw `*منشن فيديو او صوتيه *`
+let media = await q.download()
+if (!media) throw '*منشن فيديو او صوتيه*'
+let audio = await toAudio(media, 'mp4')
+if (!audio.data) throw '*منشن فيديو او صوتيه*'
+conn.sendMessage(m.chat, { audio: audio.data,  mimetype: 'audio/mpeg' }, { quoted: m })
+}
+handler.alias = ['tomp3', 'toaudio']
+handler.command = ['صوت','لصوت']
+export default handler
