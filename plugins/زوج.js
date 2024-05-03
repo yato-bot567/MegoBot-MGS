e739c64130949ffa1965e1ebdfe36a95dda91072
numberrina <167228677+numberrina@users.noreply.github.com>
2024-05-03 09:13:58 +0300

    Create زوج.js

---
+++ plugins/زوج.js
@@ -0,0 +1,16 @@
+case 'زوج': {
+            	if (isBan) return reply(mess.ban)	 			
+if (isBanChat) return reply(mess.banChat)
+            if (!m.isGroup) return replay(`${mess.group}`)
+            let member = participants.map(u => u.id)
+            let me = m.sender
+            let jodoh = member[Math.floor(Math.random() * member.length)]
+            let jawab = `「مبروك واعتبره/ا زوجك/تك 」
+
+@${me.split('@')[0]} ❤️ @${jodoh.split('@')[0]}`
+            let ments = [me, jodoh]
+            let buttons = [
+                        { buttonId: '「اضغط اقبل اذا موافق 😂」', buttonText: { displayText: 'اقبل😍' }, type: 1 }
+                    ]
+                    await XeonBotInc.sendButtonText(m.chat, buttons, jawab, XeonBotInc.user.name, m, {mentions: ments})
+                                      }
