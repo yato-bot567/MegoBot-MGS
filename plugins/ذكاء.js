f15154555e7d6a75feaf1d296b5196851cd8e2a9
numberrina <167228677+numberrina@users.noreply.github.com>
2024-05-03 09:28:16 +0300

    Create ذكاء.js

---
+++ plugins/ذكاء.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, command, text }) => {
+let love = `
+*ذكاءك ${text} انتا ذكي* *${Math.floor(Math.random() * 100)}% من 100%*
+`.trim()
+m.reply(love, null, { mentions: conn.parseMention(love) })}
+handler.help = ['Inteligencia ']
+handler.tags = ['fun']
+handler.command = /^(ذكاء)$/i
+export default handler
