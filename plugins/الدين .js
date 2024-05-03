df5b88c0b3d15380aafe23a9e56d15c61be6dd62
numberrina <167228677+numberrina@users.noreply.github.com>
2024-05-03 07:57:06 +0300

    Create الدين .js

---
+++ plugins/الدين .js
@@ -0,0 +1,30 @@
+import fs from 'fs'
+let timeout = 60000
+let poin = 500
+let handler = async (m, { conn, usedPrefix }) => {
+conn.tekateki = conn.tekateki ? conn.tekateki : {}
+let id = m.chat
+if (id in conn.tekateki) {
+conn.reply(m.chat, 'لا تزال هناك ألغاز لم يتم الرد عليها في هذه الدردشة', conn.tekateki[id][0])
+throw false
+}
+let tekateki = JSON.parse(fs.readFileSync(`./src/game/acertijo.json`))
+let json = tekateki[Math.floor(Math.random() * tekateki.length)]
+let _clue = json.filename
+let clue = _clue.replace(/[A-Za-z]/g, '_')
+let caption = `
+ⷮ *${json.path}*
+*• الوقت:* ${(timeout / 1000).toFixed(2)} ثانيه
+*• الجائزه:* +${poin} Exp
+`.trim()
+conn.tekateki[id] = [
+await conn.reply(m.chat, caption, m), json, 
+poin,
+setTimeout(async () => {
+if (conn.tekateki[id]) await conn.reply(m.chat, `انتهى الوقت!\n*الإجابة:* ${json.filename}`, conn.tekateki[id][0])
+delete conn.tekateki[id]
+}, timeout)]}
+handler.help = ['acertijo']
+handler.tags = ['game']
+handler.command = 'دين'
+export default handler
