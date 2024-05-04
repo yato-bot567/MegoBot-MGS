a8a4fbf9a9714fda61ea2e1f7dea7b24fc4d3795
numberrina <167228677+numberrina@users.noreply.github.com>
2024-05-04 18:38:26 +0300

    Create فدين.js

---
+++ plugins/فدين.js
@@ -0,0 +1,16 @@
+//API Vids:
+const handler = async (m, { conn }) => {
+  try {
+      const videoLink = "https://api.bk9.site/Islam/quranvid";
+      const videoFileName = "video.mp4";
+      await conn.sendMessage(m.chat, { video: { url: videoLink }, fileName: videoFileName, mimetype: 'video/mp4', caption: 'bk9' }, { quoted: m });
+  } catch (error) {
+      console.error(error);
+      throw "فشل في إرسال الفيديو.";
+  }
+};
+
+handler.tags = ['islam'];
+handler.command = /^(فدين)$/i;
+
+export default handler;
