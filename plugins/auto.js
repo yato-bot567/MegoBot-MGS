683a8e82df1fa36c7e1ca81a3fb6528b51b49891
numberrina <167228677+numberrina@users.noreply.github.com>
2024-05-04 20:22:18 +0300

    Create auto.js

---
+++ plugins/auto.js
@@ -0,0 +1,30 @@
+let handler = m => m; 
+
+ handler.all = async function (m) { 
+ let chat = global.db.data.chats[m.chat]; 
+ let responses; 
+ if (/^هات$/i.test(m.text)) { 
+ responses = [ 
+ 'خد🖕🏿'
+ ]; 
+ } else if (/^بوت|كاناتي|كانونه$/i.test(m.text)) { 
+     responses = [ 
+'احح كبير🤤؟'
+     ]; 
+   } else if (/^احا$/i.test(m.text)) { 
+     responses = [ 
+       '*احتين عند ام حسين*',  
+     ];
+ }else if (/^ليه$/i.test(m.text)) { 
+     responses = [ 
+       'معرفش',  
+     ];
+ }
+ if (responses) { 
+ let randomIndex = Math.floor(Math.random() * responses.length); 
+ conn.reply(m.chat, responses[randomIndex], m); 
+ } 
+ return !0 
+ }; 
+
+ export default handler;
