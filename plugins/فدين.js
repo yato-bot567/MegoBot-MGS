95b3f0b2f10fc66c34d2cb9efb23c0116c8fca8c
numberrina <167228677+numberrina@users.noreply.github.com>
2024-05-04 19:24:22 +0300

    Update فدين.js

--- plugins/فدين.js
+++ plugins/فدين.js
@@ -3,7 +3,7 @@ const handler = async (m, { conn }) => {
   try {
       const videoLink = "https://api.bk9.site/Islam/quranvid";
       const videoFileName = "video.mp4";
-      await conn.sendMessage(m.chat, { video: { url: videoLink }, fileName: videoFileName, mimetype: 'video/mp4', caption: 'bk9' }, { quoted: m });
+      await conn.sendMessage(m.chat, { video: { url: videoLink }, fileName: videoFileName, mimetype: 'video/mp4', caption: 'ابو عبدالله' }, { quoted: m });
   } catch (error) {
       console.error(error);
       throw "فشل في إرسال الفيديو.";
@@ -11,6 +11,6 @@ const handler = async (m, { conn }) => {
 };
 
 handler.tags = ['islam'];
-handler.command = /^(فدين)$/i;
+handler.command = /^(اسلاميات)$/i;
 
 export default handler;
