ca6cef81c34a255a64aa5d1043f974e5714cc896
numberrina <167228677+numberrina@users.noreply.github.com>
2024-05-12 16:49:57 +0300

    Create بضاني.js

---
+++ plugins/بضاني.js
@@ -0,0 +1,45 @@
+const handler = async (m, { conn, command, text }) => {
+  const lovePercentage = Math.floor(Math.random() * 100);
+  const isHighLove = lovePercentage >= 50;
+  const loveDescription = isHighLove ? "" : "";
+  const getRandomMessage = (messages) => messages[Math.floor(Math.random() * messages.length)];
+  const response =
+    `✦•━━━━ ∘⊰🔥⊱∘ ━━━━•✦\n` +
+    `${text}😂♥️ وينك بضاني لساتك زعلانه ` +
+    `\n✦•━━━━ ∘⊰🔥⊱∘ ━━━━•✦` +
+    ``    
+
+  async function loading() {
+var hawemod = [
+"⌯ بضاني ",
+"⌯بضاني",
+"⌯ بضاني",
+"⌯ بضاني",
+"⌯ بضاني",
+"⌯ بضاني",
+"⌯ بضاني",
+"⌯ بضاني",
+"⌯ بضاني ",
+"⌯ بضاني",
+"⌯ بضاني",
+"⌯ بضاني هتبكي",
+"⌯ بضاني ",
+"⌯ بضاني مبضونه",
+"⌯ بضاني زعلانه",
+"⌯ بضاني",
+"⌯ بضاني",
+"⌯ بضاني"
+]
+   let { key } = await conn.sendMessage(m.chat, {text: `⌯ زعلانه عيوشه`, mentions: conn.parseMention(response)}, {quoted: m})
+ for (let i = 0; i < hawemod.length; i++) {
+   await new Promise(resolve => setTimeout(resolve, 1000)); 
+   await conn.sendMessage(m.chat, {text: hawemod[i], edit: key, mentions: conn.parseMention(response)}, {quoted: m}); 
+  }
+  await conn.sendMessage(m.chat, {text: response, edit: key, mentions: conn.parseMention(response)}, {quoted: m});         
+ }
+loading()    
+};
+handler.help = ['love'];
+handler.tags = ['fun'];
+handler.command = /^(بضاني)$/i;
+export default handler;
